2edd652dd696dbc9ce617d7d78ccc4926051ac20
Athul Krishna <athulkrishna.cpd@gmail.com>
2026-06-04 00:13:00 +0530

    fix: revert emulate_prepares (breaks JSON columns), use sslmode=require for Neon

--- config/database.php
+++ config/database.php
@@ -96,10 +96,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'options' => extension_loaded('pdo_pgsql') ? [
-                \PDO::ATTR_EMULATE_PREPARES => true,
-            ] : [],
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
         'sqlsrv' => [
